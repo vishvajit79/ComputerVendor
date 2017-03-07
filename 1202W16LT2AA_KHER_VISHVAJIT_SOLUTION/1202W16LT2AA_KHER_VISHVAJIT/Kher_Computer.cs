1d8f6358577cd6836eaa808be2cc54650e0cfd33
Vishvajit Kher <vishvajit79@gmail.com>
2017-03-07 09:59:35 -0500

    Add files via upload

---
+++ 1202W16LT2AA_KHER_VISHVAJIT_SOLUTION/1202W16LT2AA_KHER_VISHVAJIT/Kher_Computer.cs
@@ -0,0 +1,83 @@
+﻿//Vishvajit Kher
+//student id - 101015270
+//comp 1202
+//crn - 48628
+//labtest 2
+//31 March 2016
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1202W16LT2A_KHER_VISHVAJIT
+{
+    class Kher_Computer
+    {
+
+        // Declaring Constants For Laptop and Desktop Computer
+        const double PRICE_OF_LAPTOP_COMPUTER = 1000;  
+        const double PRICE_OF_DESKTOP_COMPUTER = 500;
+
+        //Creating Variables
+        private string vendorName;
+        private string typeOfComputer;
+        private int quantity;
+        private double totalSales;
+
+        //Creating Properties
+        public string VendorName { get; set; }  
+        public string TypeOFComputer { get; set; } 
+        public int Quantity { get; set; }
+        public double TotalSales { get; set; }
+
+
+        //Default constructor
+        public Kher_Computer()
+        {
+
+        }
+
+        //Overloaded Constructor
+        public Kher_Computer(string _VendorName, string _TypeOfComputer, int _Quantity)
+        {
+            VendorName = _VendorName;
+            TypeOFComputer = _TypeOfComputer;
+            Quantity = _Quantity;
+        }
+
+        
+        //Method to calculate total sales using If statements
+        public void CalculateTotalSales() 
+        {
+            if (TypeOFComputer == "L" || TypeOFComputer == "l")
+            {
+                 TotalSales = Quantity * PRICE_OF_LAPTOP_COMPUTER;   
+            }
+            if(TypeOFComputer == "D" || TypeOFComputer == "d")
+            {
+                 TotalSales = Quantity * PRICE_OF_DESKTOP_COMPUTER;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            string output = "";
+            if (TypeOFComputer == "L" || TypeOFComputer == "l")
+            {
+                output = "\nVendor Name: " + VendorName + "\n" + "Type Of Computer: Laptop Computer \n" + "Price: $" + PRICE_OF_LAPTOP_COMPUTER + "\n" + "Quantity: " + Quantity + "\n" + "Total Sales: $" + TotalSales;
+
+            }
+            else
+            {
+                output = "\nVendor Name: " + VendorName + "\n" + "Type Of Computer: Desktop Computer \n" + "Price: $" + PRICE_OF_DESKTOP_COMPUTER  + "\n" +"Quantity: " + Quantity + "\n" + "Total Sales: $" + TotalSales;
+            }
+            return output;
+        }
+
+    }
+}
---
+++ 1202W16LT2AA_KHER_VISHVAJIT_SOLUTION/1202W16LT2AA_KHER_VISHVAJIT/Kher_ComputerApp.cs
@@ -0,0 +1,108 @@
+﻿//Vishvajit Kher
+//student id - 101015270
+//comp 1202
+//crn - 48628
+//labtest 2
+//31 March 2016
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1202W16LT2A_KHER_VISHVAJIT
+{
+    class Kher_ComputerApp
+    {
+        static void Main(string[] args)
+        {
+            string exit = "y"; //declaring a variable to stop the do while loop
+            do
+            {
+                string vendorName, typeOfComputer, output = ""; //variable of computer
+                int quantity;
+                
+                //changing text color for each data members
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                vendorName = VendorName();         //taking input of vendor name of computer
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                typeOfComputer = TypeOfComputer();       //taking input of type of computer
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                quantity = QuantityOfComputers();   //taking input of quantity of computers
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Kher_Computer computer1 = new Kher_Computer(vendorName, typeOfComputer, quantity);  //creating new obejct computer
+
+                computer1.CalculateTotalSales(); //calculating total sales
+                output = computer1.ToString();
+                Console.WriteLine(output);     //printing details about the computer from variable
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\n\nDo You Wish To Continue Shooping?\nPress N to Exit Shooping: ");
+                exit = Console.ReadLine();
+            } while (exit != "N" && exit != "n");
+
+            
+        }
+
+        //method to input vendor name
+        public static string VendorName()  
+        {
+            Console.Write("Enter The Name Of The Computer Vendor: ");
+            return Console.ReadLine();
+        }
+
+        //method to input computer type
+        public static string TypeOfComputer()  
+        {
+            string type;
+            do
+            {
+                Console.Write("Which Type Of Computer Do You Want -");
+                Console.Write(" Press L for Laptop Computer & D for Desktop Computer: ");
+                
+                type = Console.ReadLine();
+                if (type != "L" && type != "l" && type != "D" && type != "d")
+                {
+                    Console.WriteLine("\n\nPlease choose the  Valid Computer only !");
+                }
+            } while (type != "L" && type != "l" && type != "D" && type != "d");
+            return type;
+        }
+
+        //method to input quantity of computers
+        public static int QuantityOfComputers() 
+        {
+            int quantity;
+            do
+            {
+                Console.Write("\nPlease Input Quantity Of Computer(s): ");
+                string qty = Console.ReadLine();
+                bool result = Int32.TryParse(qty, out quantity);
+                if (result)
+                {
+                    if (quantity < 0)
+                    {
+                        quantity = 0;
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter only positive integer values.");
+                }
+            } while (true);
+
+            return quantity;
+        }
+
+    }
+}
